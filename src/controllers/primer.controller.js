40d5867bf49bcb7bac1bb09cb827a3c3bd510d05
Adrian Gutierrez <adgutierrez@ikeasistencia.com>
2023-03-22 21:31:29 -0600

    Bitacora

--- src/controllers/primer.controller.js
+++ src/controllers/primer.controller.js
@@ -126,30 +126,88 @@ export const createSocio = async (req,res) => {
     res.status(201).json(response)
 }
 
-/*export const updateSocio = async (req,res) => {
+export const updateSocio = async (req,res) => {
     
     let {nombre,apellido,telefono,edad,email} = req.body
+    let socio;
+    try {
+
+        socio = await models.socios.findOne({
+            where: {email}
+        })        
+
+        socio.nombre = nombre
+        socio.apellido = apellido
+        socio.telefono = telefono
+        socio.edad = edad
+
+        await socio.save()
+    } catch (error) {
+        console.log("Hubo un error: " + error)
+        res.status(500).json({"Error": "Hubo un error, "+ error})
+        return;
+    }
+    
+    res.status(202).json(socio)
+}
+
+export const deleteSocio = async (req,res) => {
+    
+    let {email} = req.params
+    let socio;
+    try {
+
+        socio = await models.socios.destroy({
+            where: {email}
+        })        
+
+        
+    } catch (error) {
+        console.log("Hubo un error: " + error)
+        res.status(500).json({"Error": "Hubo un error, "+ error})
+        return;
+    }
+    
+    res.status(202).json(socio)
+}
+
+export const getBarcos = async (req,res) => {
+    let response;
+    try {
+        response = await models.barcos.findAll({}) //Encuentra todos
+    } catch (error) {
+        console.log("Hubo un error: " + error)
+        res.status(500).json({"Error": "Hubo un error, "+ error})
+        return;
+    }
+    
+    res.status(200).json(response)
+}
+
+export const createBitacora = async (req,res) => {
+    let {tiempo_salida,fecha_salida,barco_id,patron_id} = req.body
+    console.log(tiempo_salida)
     let response;
     let aux;
     try {
 
-        aux = await models.socios.findAll({
-            where: {email}
+        aux = await models.bitacoras.findAll({
+            where: {barco_id,fecha_salida},
         })
 
-        if(aux.length > 0){
-            res.status(406).json({
-                "Estatus": "Este email ya se encuentra registrado"
+        if(aux.length >= 2){
+            res.status(401).json({
+                "Estatus": "El barco no puede salir mas de dos veces"
             })
             return;
         }
 
-        response = await models.socios.create({
-            nombre,
-            apellido,
-            telefono,
-            edad,
-            email
+    
+        response = await models.bitacoras.create({
+            tiempo_salida,
+            fecha_salida,
+            barco_id,
+            patron_id
         })
 
     } catch (error) {
@@ -160,4 +218,3 @@ export const createSocio = async (req,res) => {
     
     res.status(201).json(response)
 }
-*/
